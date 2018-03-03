cbb8a3a3ac1e1201c8dace805b1485faf99636a2
Ken Courville <kcourville@bluedotsolutions.com>
2018-03-03 06:15:07 -0700

    flesh out requirements; stub projects

---
+++ SOLID.Calculator/Calculator.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SOLID.Calculator
+{
+    public class Calculator
+    {
+    }
+}
---
+++ SOLID.CalculatorTests/CalculatorTest.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace SOLID.CalculatorTests
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+
+        }
+    }
+}
