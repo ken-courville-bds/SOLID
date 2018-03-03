c181ab02ad0e26cc51f3c44dfcf1a73fba22e144
Ken Courville <kcourville@bluedotsolutions.com>
2018-03-03 06:28:28 -0700

    complete iteration 1
    All requirements are fullfilled

--- SOLID.Calculator/Calculator.cs
+++ SOLID.Calculator/Calculator.cs
@@ -1,8 +1,41 @@
 ﻿using System;
 
-namespace SOLID.Calculator
+namespace SOLID.Math
 {
     public class Calculator
     {
+        private double result;
+
+        public Calculator(double value)
+        {
+            result = value;
+        }
+
+        public Calculator Input(string op, double value)
+        {
+            switch (op)
+            {
+                case "+":
+                    result += value;
+                    break;
+                case "-":
+                    result -= value;
+                    break;
+                case "*":
+                    result *= value;
+                    break;
+                case "/":
+                    result /= value;
+                    break;
+                default:
+                    throw new NotSupportedException($"Operation not supported: {op}");
+            }
+            return this;
+        }
+
+        public double Result()
+        {
+            return result;
+        }
     }
 }
--- SOLID.CalculatorTests/CalculatorTest.cs
+++ SOLID.CalculatorTests/CalculatorTest.cs
@@ -1,3 +1,4 @@
+using SOLID.Math;
 using System;
 using Xunit;
 
@@ -6,10 +7,51 @@ namespace SOLID.CalculatorTests
     public class CalculatorTest
     {
         [Fact]
-        public void Test1()
+        public void Add()
         {
-            Assert.True(true);
+            var actual = new Calculator(1)
+                .Input("+", 1)
+                .Result();
+            Assert.Equal(2, actual);
+        }
 
+        [Fact]
+        public void Substract()
+        {
+            var actual = new Calculator(1)
+                .Input("-", 1)
+                .Result();
+            Assert.Equal(0, actual);
+        }
+
+        [Fact]
+        public void Multiply()
+        {
+            var actual = new Calculator(1)
+                .Input("*", 2)
+                .Result();
+            Assert.Equal(2, actual);
+        }
+
+        [Fact]
+        public void Divide()
+        {
+            var actual = new Calculator(2)
+                .Input("/", 2)
+                .Result();
+            Assert.Equal(1, actual);
+        }
+
+        [Fact]
+        public void Chaining()
+        {
+            var actual = new Calculator(2)
+                .Input("+", 10)
+                .Input("-", 10)
+                .Input("*", 2)
+                .Input("/", 2)
+                .Result();
+            Assert.Equal(2, actual);
         }
     }
 }
